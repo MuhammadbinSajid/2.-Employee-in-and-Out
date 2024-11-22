917f45eeb6ee0b9b040979da339c23cc7026a9c8
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-11-22 16:37:33 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Create class
+class Employee:
+
+    # Initializing
+    def __init__(self):
+        print('Employee created')
+        
+    # Calling destructor
+    def __del__(self):
+        print("Destructor called")
+
+def Create_obj():
+    print('Making Object...')
+    obj = Employee()
+    print('Function End...')
+    return obj
+
+print('Calling Create_obj function...')
+obj = Create_obj()
+print('Program End...')
